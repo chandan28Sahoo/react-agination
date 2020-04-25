3606be7a0dc339ebab1426f0148b4414717ac856
Swatinavgurukul <swati18@navgurukul.org>
2020-04-25 18:49:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Pagination from "react-js-pagination";
+import axios from 'axios';
+import "bootstrap-less" 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      pagesList:[],
+      startPage:0,
+      pageCount:0,
+      page: 0
+    };
+  }
+  
+  componentDidMount() {
+    axios.get(`https://hn.algolia.com/api/v1/search_by_date?tags=story&page=${this.state.page}`)
+      .then(res => {
+        this.setState({ pagesList : this.state.pagesList.concat(res.data.hits) });
+        if (res.data.hits.length != 0) {
+          this.setInterval()
+        }
+      })
+  }
+
+
+  setInterval = () => {
+      this.setState({
+        page: this.state.page + 1
+      })
+      this.componentDidMount()
+  }
+
+  
+  handlePageChange = (pageNumber) => {
+    // console.log(`active page is ${pageNumber}`);
+    this.setState({startPage: pageNumber});
+  }
+  
+  getRenderData = () => { 
+    let visible_data = [],
+    itemsPerPage =  10
+
+    let offset = Math.ceil(this.state.startPage * itemsPerPage);
+    for (var i = 0; i < this.state.pagesList.length; i++) {
+      if (i >= offset && i < offset + itemsPerPage) {
+        visible_data.push(this.state.pagesList[i]);
+      }
+    }
+    return visible_data
+  }
+  
+  render() {
+    
+    let tabledata = this.getRenderData().map((item) => {
+    return <div>{item.title}{item.url}{item.created_at}{item.author}</div>
+    });
+
+    return (
+      <div className="ju">
+        { tabledata }
+        <Pagination className="pagination"
+          itemsCountPerPage={10}
+          totalItemsCount={this.state.pagesList.length - 10}
+          pageRangeDisplayed={10}
+          activePage={this.state.startPage}
+          initialPage={this.state.startPage}
+          pageCount={this.state.pageCount}
+          onChange={this.handlePageChange}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
